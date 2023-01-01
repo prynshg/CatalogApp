3d8f2c79fa0b9b69498b7ec2143093ff932093d3
IamAgroceryBag <priyanshgarg709@gmail.com>
2023-01-01 23:16:46 +0530

    day 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'HomePage.dart';
+
 void main(){
   runApp(MyApp());
 }
@@ -9,10 +11,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-      home: Container(
-        child: Text("Welcome to 30 days of flutter"),
-      ),
+      home: HomePage(),
     );
   }
 }
