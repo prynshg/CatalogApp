e76316a53c550cfe1949c94f829fb39741534466
IamAgroceryBag <priyanshgarg709@gmail.com>
2023-01-04 13:28:08 +0530

    day 12 completed

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertest/widgets/drawer.dart';
+import 'package:fluttertest/widgets/item_widget.dart';
+import 'package:fluttertest/models/catalog.dart';
 class HomePage extends StatelessWidget {
   final int days=30;
   final String name="Codepur";
@@ -7,18 +9,22 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final dummyList=List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text(
             "Catalog App",
         ),
       ),
-      body: Center(
-        child: Container(
-        child: Text("Welcome to $days days of flutter by $name"),
-        ),
+      body:Padding(padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+        itemCount: dummyList.length,
+        itemBuilder: (context,index) {
+        return ItemWidget(item: dummyList[index]);
+        },
       ),
-      drawer: MyDrawer(),
+      ),
+    drawer: MyDrawer(),
       );
   }
 }
