b469343017f3cee0167cd4753b1d71da9081d0b8
IamAgroceryBag <priyanshgarg709@gmail.com>
2023-01-05 23:12:16 +0530

    day 15 completed

--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -4,7 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 class MyTheme{
   static ThemeData lightTheme(BuildContext context)=>ThemeData(
         primarySwatch: Colors.amber,
-        fontFamily: GoogleFonts.lato().fontFamily,
+        fontFamily: GoogleFonts.poppins().fontFamily,
         appBarTheme: AppBarTheme(
           color: Colors.amber,
           elevation: 0.0,
@@ -16,4 +16,7 @@ class MyTheme{
   static ThemeData darkTheme(BuildContext context)=>ThemeData(
      brightness: Brightness.dark,
   );
+
+  static Color creamColor=Color(0xfff5f5f5);
+  static Color darkBlueishColor=Color(0xff403b58);
 }
